b524db34ae470ff453d68a8cea64c8eea7b83ad4
juskuc <justkucinskas@gmail.com>
2021-01-17 16:52:52 +0200

    Shader functions

--- fragment_core.glsl
+++ fragment_core.glsl
@@ -1,5 +1,14 @@
 #version 440
 
+struct Material
+{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	sampler2D diffuseTex;
+	sampler2D specularTex;
+};
+
 in vec3 vs_position;
 in vec3 vs_color;
 in vec2 vs_textcoord;
@@ -7,38 +16,53 @@ in vec3 vs_normal;
 
 out vec4 fs_color;
 
-uniform sampler2D texture0;
-uniform sampler2D texture1;
 
+// Uniforms
+uniform Material material;
 uniform vec3 lightPos0;
 uniform vec3 cameraPos;
 
-void main()
+// Functions
+vec3 calculateAmbient(Material material)
 {
-	//fs_color = vec4(vs_color, 1.f);
-	//fs_color = texture(texture0, vs_textcoord) * texture(texture1, vs_textcoord) * vec4(vs_color, 1.f);
-	//fs_color = mix(texture(texture0, vs_textcoord), texture(texture1, vs_textcoord), 0.67);
-	
-	// Ambient light
-	vec3 ambientLight = vec3(0.1f, 0.1f, 0.1f);
+	return material.ambient;
+}
 
-	// Diffuse light
+vec3 calculateDiffuse(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0)
+{
 	vec3 posToLightDirVec = normalize(vs_position - lightPos0);
-	vec3 diffuseColor = vec3(1.f, 1.f, 1.f);
 	float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
-	vec3 diffuseFinal = diffuseColor * diffuse;
+	vec3 diffuseFinal = material.diffuse * diffuse;
 
-	// Specular light
+	return diffuseFinal;
+}
+
+vec3 calculateSpecular(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0, vec3 cameraPos)
+{
 	vec3 lightToPosDirVec = normalize(lightPos0 - vs_position);
 	vec3 reflectDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
 	vec3 posToViewDirVec = normalize(vs_position - cameraPos);
 	float specularConstant = pow(max(dot(posToViewDirVec, reflectDirVec), 0), 30);
-	vec3 specularFinal = vec3(1.f, 1.f, 1.f) * specularConstant;
+	vec3 specularFinal = material.specular * specularConstant;
+
+	return specularFinal;
+}
+
+void main()
+{
+	// Ambient light
+	vec3 ambientFinal = calculateAmbient(material);
+
+	// Diffuse light
+	vec3 diffuseFinal = calculateDiffuse(material, vs_position, vs_normal, lightPos0);
+
+	// Specular light
+	vec3 specularFinal = calculateSpecular(material, vs_position, vs_normal, lightPos0, cameraPos);
 
 	// Attenuation
 
 	// Final light
 	fs_color = 
-	texture(texture0, vs_textcoord) * vec4(vs_color, 1.f)
-	* (vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
+	texture(material.diffuseTex, vs_textcoord) * vec4(vs_color, 1.f) *
+	(vec4(ambientFinal, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
 }
--- main.cpp
+++ main.cpp
@@ -117,7 +117,7 @@ int main()
 	// SHADER INIT
 	Shader core_program("vertex_core.glsl", "fragment_core.glsl");
 
-	// MODEL
+	// MODEL MESH
 
 	// VAO, VBO, EBO
 	// GEN VAO AND BIND
@@ -164,6 +164,9 @@ int main()
 
 	// TEXTURE 1
 	Texture texture1("Images/Texture1.png", GL_TEXTURE_2D, 1);
+
+	// MATERIAL 0
+	Material material0(glm::vec3(0.1f), glm::vec3(1.f), glm::vec3(1.f), texture0.getTextureUnit(), texture1.getTextureUnit());
 
 	// INIT MATRICES
 	glm::vec3 position(0.f);
@@ -226,6 +229,7 @@ int main()
 		// Update uniforms
 		core_program.set1i(texture0.getTextureUnit(), "texture0");
 		core_program.set1i(texture1.getTextureUnit(), "texture1");
+		material0.sendToShader(core_program);
 
 		ModelMatrix = glm::translate(ModelMatrix, position);
 		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(rotation.x), glm::vec3(1.f, 0.f, 0.f));
